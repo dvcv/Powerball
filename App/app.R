8512241c9175d51130e09b1c45d8a6e89bbbcdef
David Chavez <dvcv1992@gmail.com>
2016-08-29 09:54:59 -0700

    Updated app

--- App/app.R
+++ App/app.R
@@ -1,30 +1,41 @@
 library(shiny)
+library(shinythemes)
+
 ui <- fluidPage(
+  theme = shinytheme("spacelab"),
   headerPanel("Powerball"),
-  textOutput("intro"),
-  textOutput("howManyTickets"),
-  sliderInput("tickets", "Games", 1, 10400, 1000,5),
-  actionButton("play", "Play"),
-  plotOutput("plot"),
-  plotOutput("plot1"),
-  plotOutput("plot2"),
-  plotOutput("plot3"),
-  titlePanel("Best Ticket"),
-  textOutput("text1"),
-  titlePanel("Summary"),
-  textOutput("text2")
-  
+  #textOutput("intro"),
+  sidebarLayout(
+    
+    sidebarPanel(
+      textOutput("intro"),
+      sliderInput("tickets", "Games", 1, 10400, 100,100),
+      numericInput("tickets2", NULL, 1, 1,10400,1),
+      helpText("Note: Only values between 1 and 10400 are accepted."),
+      actionButton("play", "Play")
+    ),mainPanel(tabsetPanel(
+      tabPanel("Win Loss Comparison",plotOutput("plot1")),
+      tabPanel("Winning Tickets",plotOutput("plot2")),
+      tabPanel("Ticket Winnings",plotOutput("plot3")),
+      tabPanel("Spent/Won/Lost",plotOutput("plot4")),
+      tabPanel("Best Ticket",verbatimTextOutput("text1")),
+      tabPanel("Summary",verbatimTextOutput("text2"))
+      
+   )
+  ,"        ")
+ )
 )
 
 
 server <- function(input, output, session) {
   output$intro = renderText(
-    print("This is a simple powerball simulater representing many games played over a lifetime where 1 ticket was played per game. 
-          Assuming you played quickpick numbers every time.")
+    print("This is a simple powerball simulator representing many games played over a lifetime. Assuming
+          one ticket was played per game using quickpick numbers, how many games of powerball do you wish to play?")
     )
   
-  output$howManyTickets = renderText(
-    print("How many games of powerball do you wish to play?"))
+  observe({
+    updateSliderInput(session,"tickets",value=input$tickets2)
+  })
   
   play <- eventReactive(input$play, {
     input$tickets
@@ -98,21 +109,21 @@ server <- function(input, output, session) {
         win.ticket[[i]]=ticket[[i]]
       }
     }
-   win.ticket<<-split(unlist(win.ticket), ceiling(seq_along(unlist(win.ticket))/6))
-   winnings<<-sum.5w.1r*GRANDPRIZE+sum.5w*1000000+sum.4w.1r*50000+sum.4w*100+sum.3w.1r*100+sum.3w*7+sum.2w.1r*7+sum.1w.1r*4+sum.1r*4
-   sum.5w.1r<<-sum.5w.1r
-   sum.5w<<-sum.5w
-   sum.4w.1r<<-sum.4w.1r
-   sum.4w<<-sum.4w
-   sum.3w.1r<<-sum.3w.1r
-   sum.3w<<-sum.3w
-   sum.2w.1r<<-sum.2w.1r
-   sum.1w.1r<<-sum.1w.1r
-   sum.1r<<-sum.1r
-   
+    win.ticket<<-split(unlist(win.ticket), ceiling(seq_along(unlist(win.ticket))/6))
+    winnings<<-sum.5w.1r*GRANDPRIZE+sum.5w*1000000+sum.4w.1r*50000+sum.4w*100+sum.3w.1r*100+sum.3w*7+sum.2w.1r*7+sum.1w.1r*4+sum.1r*4
+    sum.5w.1r<<-sum.5w.1r
+    sum.5w<<-sum.5w
+    sum.4w.1r<<-sum.4w.1r
+    sum.4w<<-sum.4w
+    sum.3w.1r<<-sum.3w.1r
+    sum.3w<<-sum.3w
+    sum.2w.1r<<-sum.2w.1r
+    sum.1w.1r<<-sum.1w.1r
+    sum.1r<<-sum.1r
+    
   })
   
-  output$plot = renderPlot({
+  output$plot1 = renderPlot({
     data()
     x=c(sum.5w.1r,sum.5w,sum.4w.1r,sum.4w,sum.3w.1r,sum.3w,sum.2w.1r,sum.1w.1r,sum.1r)
     x = sum(x)
@@ -120,21 +131,27 @@ server <- function(input, output, session) {
     
   })
   
-  output$plot1 = renderPlot({
+  output$plot2 = renderPlot({
     data()
     x=c(sum.5w.1r,sum.5w,sum.4w.1r,sum.4w,sum.3w.1r,sum.3w,sum.2w.1r,sum.1w.1r,sum.1r)
     x=c(x,sum(x))
-    barplot(x,las=2,names.arg=c("5W 1R","5W","4W 1R","4W","3W 1R","3W","2W 1R","1W 1R","1R","Total"),col=rainbow(9),main=paste("Winning Tickets"),ylab="Winning Tickets")
-  })
-  
-  output$plot2 = renderPlot({
-    data()
-    y=c("Grand Prize","1,000,000","50,000","100","100","7","7","4","4","Total")
-    x=c(sum.5w.1r*GRANDPRIZE, sum.5w*1000000, sum.4w.1r*50000, sum.4w*100,sum.3w.1r*100, sum.3w*7, sum.2w.1r*7, sum.1w.1r*4, sum.1r*4,winnings)
-    barplot(x,las=2,names.arg=c("5W 1R","5W","4W 1R","4W","3W 1R","3W","2W 1R","1W 1R","1R","Total"),col=rainbow(9),main=paste("Ticket Winnings"),ylab="Money Won")
+    if(sum(x)==0){
+      barplot(x,axes = F, ylim = c(0,1),las=2,names.arg=c("5W 1R","5W","4W 1R","4W","3W 1R","3W","2W 1R","1W 1R","1R","Total"),col=rainbow(9),main=paste("Winning Tickets"),ylab="Winning Tickets")
+      axis(side = 2, at = c(0,1))   
+   }else barplot(x,las=2,names.arg=c("5W 1R","5W","4W 1R","4W","3W 1R","3W","2W 1R","1W 1R","1R","Total"),col=rainbow(9),main=paste("Winning Tickets"),ylab="Winning Tickets")
   })
   
   output$plot3 = renderPlot({
+    data()
+    y=c("Grand Prize","1,000,000","50,000","100","100","7","7","4","4","Total")
+    x=c(sum.5w.1r*GRANDPRIZE, sum.5w*1000000, sum.4w.1r*50000, sum.4w*100,sum.3w.1r*100, sum.3w*7, sum.2w.1r*7, sum.1w.1r*4, sum.1r*4,winnings)
+    if(winnings==0){
+      barplot(x,axes = F,ylim = c(0,1),las=2,names.arg=c("5W 1R","5W","4W 1R","4W","3W 1R","3W","2W 1R","1W 1R","1R","Total"),col=rainbow(9),main=paste("Ticket Winnings"),ylab="Money Won")
+      axis(side = 2, at = c(0,1))
+    }else barplot(x,las=2,names.arg=c("5W 1R","5W","4W 1R","4W","3W 1R","3W","2W 1R","1W 1R","1R","Total"),col=rainbow(9),main=paste("Ticket Winnings"),ylab="Money Won")
+  })
+  
+  output$plot4 = renderPlot({
     data()
     n = play()
     barplot(c(2*n,winnings,abs(2*n-winnings)),col=c("green","red","gray"),names.arg=c("Spent","Won","Lost"),ylab="Money")
@@ -148,47 +165,47 @@ server <- function(input, output, session) {
     w=best.ticket[6]
     best.ticket[6]=q
     best.ticket[7]=w
-      la=TRUE
+    la=TRUE
     while(la){
       if(length(which(best.ticket==0))==9){
-        cat("No Tickets Won")
+        cat("\n","No Tickets Won","\n","\n")
         break
       }
       best.ticket=which(best.ticket!=0)[1]
       if (best.ticket==1){
-        cat("Your best ticket had  5 Whites + Powerball Matched ($Grand Prize)")
+        cat("\n","Your best ticket had  5 Whites + Powerball Matched ($Grand Prize)","\n","\n")
         break
       }
       if(best.ticket==2){ 
-        cat("Your best ticket had  5 Whites Matched ($1,000,000")
+        cat("\n","Your best ticket had  5 Whites Matched ($1,000,000","\n","\n")
         break
       }
       if(best.ticket==3){ 
-        cat("Your best ticket had  4 Whites + Powerball Matched ($50,000)")
+        cat("\n","Your best ticket had  4 Whites + Powerball Matched ($50,000)","\n","\n")
         break
       }
       if(best.ticket==4){ 
-        cat("Your best ticket had  4 Whites Matched ($100)")
+        cat("\n","Your best ticket had  4 Whites Matched ($100)","\n","\n")
         break
       }
       if(best.ticket==5){ 
-        cat("Your best ticket had  3 Whites + Powerball Matched ($100)")
+        cat("\n","Your best ticket had  3 Whites + Powerball Matched ($100)","\n","\n")
         break
       }
       if(best.ticket==6){ 
-        cat("Your best ticket had  2 Whites + Powerball Matched ($7)")
+        cat("\n","Your best ticket had  2 Whites + Powerball Matched ($7)","\n","\n")
         break
       }
       if(best.ticket==7){
-        cat("Your best ticket had  3 Whites Matched ($7)")
+        cat("\n","Your best ticket had  3 Whites Matched ($7)","\n","\n")
         break
       }
       if(best.ticket==8){ 
-        cat("Your best ticket had  1 White + Powerball Matched ($4)")
+        cat("\n","Your best ticket had  1 White + Powerball Matched ($4)","\n","\n")
         break
       }
       if(best.ticket==9){ 
-        cat("Your best ticket had  Power Ball Matched ($4)")
+        cat("\n","Your best ticket had  Power Ball Matched ($4)","\n","\n")
         break
       }
     }
@@ -196,38 +213,63 @@ server <- function(input, output, session) {
   
   output$text2 = renderPrint({
     data()
+    winning2 = c(sum.5w.1r*GRANDPRIZE, sum.5w*1000000, sum.4w.1r*50000, sum.4w*100, sum.3w.1r*100, sum.3w*7, sum.2w.1r*7, sum.1w.1r*4, sum.1r*4)
     x=c(sum.5w.1r,sum.5w,sum.4w.1r,sum.4w,sum.3w.1r,sum.3w,sum.2w.1r,sum.1w.1r,sum.1r)
     x=c(sum(x))
     n = play()
     if(as.integer((2*n)-winnings)>=0){
-    cat(  "In your lifetime you spent $", as.integer(2*n),"and won $", as.integer(winnings),"", ".\n",
-          "From the",  n , "ticket(s) you purchased", x, "ticket(s) were winners.","\n",
-          "You lost $", (2*n)-winnings,".","\n","\n",
-          "You got 5 White + Powerball   ", sum.5w.1r, "time(s).","\n",
-          "You got 5 White               ", sum.5w, "time(s).","\n",
-          "You got 4 White + Powerball   ", sum.4w.1r, "time(s).","\n",
-          "You got 4 White               ", sum.4w, "time(s),","\n",
-          "You got 3 White + Powerball   ", sum.3w.1r, "time(s).","\n",
-          "You got 3 White               ", sum.3w, "time(s),","\n",
-          "You got 2 White + Powerball   ", sum.2w.1r, "time(s).","\n",
-          "You got 1 White + Powerball   ", sum.1w.1r, "time(s).","\n",
-          "You got only Powerball             ", sum.1r, "time(s)."
-      )
+      cat(  "\n", "In your lifetime you spent $", as.integer(2*n),", won $", as.integer(winnings),
+            ", and lost $", (2*n)-winnings,".","\n",
+            "From the",  n , "ticket(s) you purchased", x, "ticket(s) were winners.","\n","\n")
+            m = matrix(c(
+            sum.5w.1r, winning2[1],  
+            sum.5w,    winning2[2],  
+            sum.4w.1r, winning2[3],  
+            sum.4w,    winning2[4],  
+            sum.3w.1r, winning2[5],  
+            sum.3w,    winning2[6],  
+            sum.2w.1r, winning2[7],  
+            sum.1w.1r, winning2[8],  
+            sum.1r,    winning2[9]     
+            ),nrow = 9,ncol = 2, byrow = T)
+            colnames(m) <- c( "Freq", "Won")
+            rownames(m) <- c("5 White+Powerball",    
+                             "5 White",  
+                             "4 White+Powerball",  
+                             "4 White",    
+                             "3 White+Powerball",   
+                             "3 White",     
+                             "2 White+Powerball",      
+                             "1 White+Powerball",    
+                             "Powerball ")
+            as.table(m)
     }else{
-      cat(  "In your lifetime you spent $", as.integer(2*n),"and won $", as.integer(winnings),"", ".\n",
-            "From the",  n , "ticket(s) you purchased", x, "ticket(s) were winners.","\n",
-            "You won $", -((2*n)-winnings),".","\n","\n",
-            "You got 5 White + Powerball   ", sum.5w.1r, "time(s).","\n",
-            "You got 5 White               ", sum.5w, "time(s).","\n",
-            "You got 4 White + Powerball   ", sum.4w.1r, "time(s).","\n",
-            "You got 4 White               ", sum.4w, "time(s),","\n",
-            "You got 3 White + Powerball   ", sum.3w.1r, "time(s).","\n",
-            "You got 3 White               ", sum.3w, "time(s),","\n",
-            "You got 2 White + Powerball   ", sum.2w.1r, "time(s).","\n",
-            "You got 1 White + Powerball   ", sum.1w.1r, "time(s).","\n",
-            "You got only Powerball             ", sum.1r, "time(s)."
-      )
-      
+      cat(  "\n", "In your lifetime you spent $", as.integer(2*n),", won $", as.integer(winnings),
+            ", and profited $", -((2*n)-winnings),".","\n",
+            "From the",  n , "ticket(s) you purchased", x, "ticket(s) were winners.","\n","\n")
+              m = matrix(c(
+              sum.5w.1r, winning2[1],  
+              sum.5w,    winning2[2],  
+              sum.4w.1r, winning2[3],  
+              sum.4w,    winning2[4],  
+              sum.3w.1r, winning2[5],  
+              sum.3w,    winning2[6],  
+              sum.2w.1r, winning2[7],  
+              sum.1w.1r, winning2[8],  
+              sum.1r,    winning2[9]     
+            ),nrow = 9,ncol = 2, byrow = T)
+            colnames(m) <- c( "Freq", "Won")
+            rownames(m) <- c("5 White+Powerball",    
+                             "5 White",  
+                             "4 White+Powerball",  
+                             "4 White",    
+                             "3 White+Powerball",   
+                             "3 White",     
+                             "2 White+Powerball",      
+                             "1 White+Powerball",    
+                             "Powerball ")
+            as.table(m)
+  
     }
   })
   
